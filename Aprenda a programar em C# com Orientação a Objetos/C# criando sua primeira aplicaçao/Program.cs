ac09b3fa861a2601878f242acc09af3833fd6caa
GuiSousa2910 <guilherme.agostinho@sptech.school>
2024-05-07 10:00:43 -0300

    c#

--- Aprenda a programar em C# com Orientação a Objetos/C# criando sua primeira aplicaçao/Program.cs
+++ Aprenda a programar em C# com Orientação a Objetos/C# criando sua primeira aplicaçao/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Screen Sound
+
+string msgBoasVindas = "Seja bem vindo ao Screen Sound";
+Console.WriteLine(msgBoasVindas);
---
+++ Aprenda a programar em C# com Orientação a Objetos/C# criando sua primeira aplicaçao/desafio1-1.cs
@@ -0,0 +1,9 @@
+// Criar uma variável chamada curso, e guarde nela o nome do curso estudado.
+
+string curso = "ADS";
+Console.WriteLine(curso);
+
+string nome = "Guilherme";
+string sobrenome = "Sousa";
+
+Console.WriteLine(nome + " " + sobrenome);
