618719c35bceb386fb46eb4188f00dec5adcb94e
GuiSousa2910 <122701467+GuiSousa2910@users.noreply.github.com>
2024-08-14 19:38:41 -0300

    arrumando pastas

--- Python para Data Science/Python para Data Science trabalhando com funçoes, estruturas de dados e exceçoes/importando_bibliotecas.py
+++ Python para Data Science/Python para Data Science trabalhando com funçoes, estruturas de dados e exceçoes/importando_bibliotecas.py
@@ -4,7 +4,7 @@ from random import choices
 estudantes = ['Joao', 'Maria', 'Gui']
 notas = [8.5, 9, 6.5]
 plt.bar(x = estudantes, height=notas)
-# plt.show()
+plt.show()
 
 estudantes_2 = ['Joao', 'Maria', 'Gui', 'Ana', 'Duda']
 estudante = choices(estudantes_2)
